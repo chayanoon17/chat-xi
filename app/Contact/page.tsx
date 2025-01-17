eee8876c0dff3445afa76affefc4818e6a41afc9
Chayanoon Aphaiwong <kendev@MacBook-Air-khxng-Chayanoon.local>
2025-01-17 22:24:57 +0700

    Update Read me

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { AnimatedPinDemo } from '../components/UI/AnimatedPinDemo';
 import Navbar from '../components/UI/Navbar';
 import { TextGenerateEffectDemo } from '../components/UI/TextGenerateEffectDemo';
+import Footer from '../components/Footer/Footer';
 
 
 
@@ -11,12 +12,16 @@ const ContactUs: React.FC = () => {
 
 
   return (
+    <>
     <div className="bg-black min-h-screen text-white flex flex-col items-center">
       <Navbar/>
       <div className=''></div>
       <TextGenerateEffectDemo/>
       <AnimatedPinDemo/>
     </div>
+    <Footer/>
+    </>
+    
   );
 };
 
