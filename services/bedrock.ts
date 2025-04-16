09b7c70bea0f2488d96dbcf117660c91699758a3
Chayanoon Aphaiwong <kendev@MacBook-Air-khxng-Chayanoon.local>
2025-04-16 17:19:34 +0700

    runtime

--- services/bedrock.ts
+++ services/bedrock.ts
@@ -8,7 +8,6 @@ if (!region || !accessKeyId || !secretAccessKey) {
   throw new Error('AWS credentials are not set in the environment variables');
 }
 
-
 // ตั้งค่า AWS Bedrock
 const llm = new ChatBedrockConverse({
   model: 'anthropic.claude-3-5-sonnet-20240620-v1:0',
