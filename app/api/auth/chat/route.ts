0cef1aec19926fc8a29949c567221908eb86cecb
Chayanoon Aphaiwong <kendev@MacBook-Air-khxng-Chayanoon.local>
2025-04-17 07:13:41 +0700

    update

--- app/api/auth/chat/route.ts
+++ app/api/auth/chat/route.ts
@@ -1,5 +1,5 @@
 // api/auth/chat/route.ts
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 import { NextRequest, NextResponse } from 'next/server';
 import { getLLMStream } from '../../../../services/bedrock';
 import prisma from '../../../../lib/prisma';
